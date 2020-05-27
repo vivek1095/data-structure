15a4bfa8adbf2c6734965af17081d235225f2253
vivek1095 <vivekpatel191@yahoo.com>
2020-05-27 10:21:44 +0530

    Added more operations

--- Linked List/LinkedList.java
+++ Linked List/LinkedList.java
@@ -1,24 +1,152 @@
 public class LinkedList{
-    
-    public static <T> Node<T> add(Node<T> head,T value) {
+    public static <T> Node<T> add(Node<T> head,final T value) {
         if(head == null){
             head = new Node<T>(value);
         }
         else{
-            Node<T> newNode = new Node<T>(value);
+            final Node<T> newNode = new Node<T>(value);
             head.setNext(newNode);
         }
         return head;
     }
 
-    public static <T> void display(Node<T> head){
+    public static <T> Node<T> addFirst(Node<T> head,final T value){
+        if(head == null){
+            head = new Node<T>(value);
+        }
+        else{
+            final Node<T> newNode = new Node<T>(value);
+            newNode.setNext(head);
+            head = newNode;
+        }
+        return head;
+    }
+
+    public static <T> Node<T> addLast(Node<T> head,final T value){
+        if(head == null){
+            head = new Node<T>(value);
+        }
+        else{
+            final Node<T> newNode = new Node<T>(value);
+            Node<T> tempNode = head;
+
+            while(tempNode.getNext() != null){
+                tempNode = tempNode.getNext();
+            }
+            tempNode.setNext(newNode);
+        }
+        return head;
+    }
+
+    public static <T> Node<T> addAfter(Node<T> head,final T indexVal,final T value){
+        final Node<T> newNode = new Node<T>(value);
+        if(head.getValue() == indexVal){
+            newNode.setNext(head.getNext());
+            head.setNext(newNode);
+        }
+        else{
+            Node<T> tempNode = head;
+
+            while(tempNode != null && tempNode.getValue()!= indexVal ){
+                tempNode = tempNode.getNext();
+            }
+            if(tempNode == null){
+                System.out.println("Index value does not exist " + indexVal);
+            }
+            else{
+                newNode.setNext(tempNode.getNext());
+                tempNode.setNext(newNode);
+            }
+        }
+        return head;
+    }
+
+    public static <T> Node<T> addBefore(Node<T> head, T indexVal, T value){
+        Node<T> newNode = new Node<T>(value);
+        if(head.getValue() == indexVal){
+            newNode.setNext(head);
+            head = newNode;
+        }
+        else{
+            Node<T> curr = head;
+            Node<T> prev = null;
+
+            while(curr != null && curr.getValue()!=indexVal){
+                prev = curr;
+                curr = curr.getNext();
+            }
+            if(curr == null){
+                System.out.println("Index value does not exist " + indexVal);
+            }
+            else{
+                newNode.setNext(prev.getNext());
+                prev.setNext(newNode);
+            }
+        }
+        return head;
+    }
+
+    public static <T> int search(Node<T> head, T value){
+        int index = 0;
+        if(head == null){
+            return -1;
+        }
+        else if(head.getValue() == value){
+            return 0;
+        }
+        else{
+            Node<T> tempNode = head;
+            while(tempNode != null && tempNode.getValue() != value){
+                tempNode = tempNode.getNext();
+                index++;
+            }
+            if(tempNode == null){
+                return -1;
+            }
+            else{
+                return index;
+            }
+        }
+    }
+    public static <T> Node<T> delete(Node<T> head,T value){
+        if(head.getValue() == value){
+            Node<T> temp = head;
+            head = null;
+            head = temp.getNext();
+        }
+        else{
+            Node<T> curr = head;
+            Node<T> prev = null;
+            while(curr != null && curr.getValue() != value){
+                prev = curr;
+                curr = curr.getNext();
+            }        
+            if(curr == null){
+                System.out.println("Value does not exist");
+            }  
+            else{
+                prev.setNext(curr.getNext());
+            } 
+        }
+        return head;
+    }
+
+    public static <T> Node<T> reverse(Node<T> head){
+        Node<T> temp = head;
+        while(temp.getNext() != null){
+            temp = temp.getNext();
+        }
+        return head;
+    }
+    public static <T> void display(final Node<T> head){
         if (null == head){
             System.out.println("List is empty");
         }
         else{
             Node<T> tempNode = head;
+            System.out.println();
             while(tempNode != null){
-                System.out.println(tempNode.getValue() + "-->");
+                System.out.print(tempNode.getValue() + "--> ");
                 tempNode = tempNode.getNext();
             }
         }
--- Linked List/MainClass.java
+++ Linked List/MainClass.java
@@ -2,8 +2,38 @@ public class MainClass{
     public static void main(String args[]){
         Node<Number> head = null;
 
-        head = LinkedList.add(head, 123);
+        // number linkedlist
+        head = LinkedList.add(head, 2);
+        head = LinkedList.add(head, 3);
 
+        head = LinkedList.addFirst(head, 1);    
+        head = LinkedList.addLast(head, 4);
+        head = LinkedList.addLast(head, 5);
+
+        head = LinkedList.addAfter(head, 5, 7);
+        
+        head = LinkedList.addBefore(head, 1, 0);
+        head = LinkedList.addBefore(head, 7, 6);
+        head = LinkedList.addBefore(head, 10, 5);
+
+        System.out.println("Number linked list");
         LinkedList.display(head);
+        
+        int index = LinkedList.search(head,5);
+        System.out.println("\nElement found on index " + index);
+
+        head = LinkedList.delete(head, 0);
+        LinkedList.display(head);
+
+        head = LinkedList.delete(head, 2);
+        LinkedList.display(head);
+
+        // string linkedlist
+        Node<String> head2 = null;
+        head2 = LinkedList.add(head2, "abc");
+        head2 = LinkedList.add(head2, "xyz");
+
+        System.out.println("\nString linked list");
+        LinkedList.display(head2);
     }
 }
